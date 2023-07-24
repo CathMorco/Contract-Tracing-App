928aa4a0e7df3d38445816b124e320627acccb60
unknown <morco.cathleenjan.12192003@gmail.com>
2023-07-25 00:20:23 +0800

    Added new text files via the program for example text files

--- Contact_Tracing_App.py
+++ Contact_Tracing_App.py
@@ -6,8 +6,6 @@ from PyQt5.QtGui import QIntValidator, QRegExpValidator
 from PyQt5.QtCore import QRegExp
 import re
 
-
-
 class GUI(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
